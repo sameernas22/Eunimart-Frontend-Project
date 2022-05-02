208ae9d30dca71ce09c38735367bc4edfad89f5a
Sameer Khan <66109509+sameernas22@users.noreply.github.com>
2022-05-02 14:56:13 +0530

    Dynamic divs

--- src/Users.js
+++ src/Users.js
@@ -13,7 +13,7 @@ const Users = () => {
                 setItems(response.data)
             })
     }, [url])
-    
+
     if(items){
         
         let item = items.data;
@@ -29,59 +29,49 @@ const Users = () => {
                 
             );
         }
+
+        
         else if(!id){
+            const CreateDivs = ({i}) =>{
+                let item=items.data;
+                if(i==1){
+                    return(
+                        <Link to={i.toString()} className='linkss' >
+                            <div className='card'>
+                            <img  src = {item[0].avatar} alt='image1'></img>
+                            <h1>First Name: {item[0].first_name}</h1>
+                            <h1>Last Name: {item[0].last_name}</h1>
+                            <h5>Email: {item[0].email}</h5>
+                        </div>
+                        </Link>
+                    )
+                }
+                else{
+                    
+                    
+                    return(
+                        <>
+                        <CreateDivs i = {i-1}/>
+                        <Link to={i.toString()} className='linkss' >
+                            <div className='card'>
+                            <img  src = {item[i-1].avatar} alt='image1'></img>
+                            <h1>First Name: {item[i-1].first_name}</h1>
+                            <h1>Last Name: {item[i-1].last_name}</h1>
+                            <h5>Email: {item[i-1].email}</h5>
+                        </div>
+                        </Link>
+                        
+                        </>
+                    )
+                }
+            }
+            
             return(
-                <div className='cards'>
-                <Link to='1'className='linkss' >
-                    <div className='card'>
-                    <img  src = {item[0].avatar} alt='image1'></img>
-                    <h1>First Name: {item[0].first_name}</h1>
-                    <h1>Last Name: {item[0].last_name}</h1>
-                    <h5>Email: {item[0].email}</h5>
-                </div>
-                </Link>
-                <Link to='2' className='linkss' >
-                <div className='card'>
-                <img  src = {item[1].avatar } alt='image2'></img>
-                    <h1>First Name: {item[1].first_name}</h1>
-                    <h1>Last Name: {item[1].last_name}</h1>
-                    <h5>Email: {item[1].email}</h5>
-                </div>
-                </Link>
-                <Link to='3' className='linkss' >
-                <div className='card'>
-                <img  src = {item[2].avatar} alt='image3'></img>
-                    <h1>First Name: {item[2].first_name}</h1>
-                    <h1>Last Name: {item[2].last_name}</h1>
-                    <h5>Email: {item[2].email}</h5>
-                </div>
-                </Link>
-                <Link to='4' className='linkss' >
-                <div className='card'>
-                <img  src = {item[3].avatar} alt='image4'></img>
-                    <h1>First Name: {item[3].first_name}</h1>
-                    <h1>Last Name: {item[3].last_name}</h1>
-                    <h5>Email: {item[3].email}</h5>
-                </div>
-                </Link>
-                <Link to='5' className='linkss' >
-                <div className='card'>
-                <img  src = {item[4].avatar} alt='image5'></img>
-                    <h1>First Name: {item[4].first_name}</h1>
-                    <h1>Last Name: {item[4].last_name}</h1>
-                    <h5>Email: {item[4].email}</h5>
-                </div>
-                </Link>
-                <Link to='6' className='linkss'>
-                <div className='card' >
-                <img  src = {item[5].avatar} alt='image6'></img>
-                    <h1>First Name: {item[5].first_name}</h1>
-                    <h1>Last Name: {item[5].last_name}</h1>
-                    <h5>Email: {item[5].email}</h5>
-                </div>
-                </Link>
-                
+            <div className='cards'>
+        
+        <CreateDivs i={items.per_page}/>
             </div>
+            
             );
         }
     
